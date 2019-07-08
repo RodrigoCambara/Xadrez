866eef0d78f70e86598082f14e4219cdc2e38da3
RodrigoCambara <rodrigonasc78@gmail.com>
2019-07-08 19:50:39 -0300

    commit-4_chess layer and print board

--- Xadrez/src/app/Program.java
+++ Xadrez/src/app/Program.java
@@ -1,13 +1,13 @@
 package app;
 
-import boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-	
-		Board board = new Board(8, 8);
-		
+
+		ChessMatch chessmatch = new ChessMatch();
+		UI.printBoard(chessmatch.getPieces());
 
 	}
 
